6775abc56b95f01c0f7e61665077c08a22e5e625
Lee Naeun <nanni4287@gmail.com>
2022-01-10 15:39:25 +0900

    [Server] ec2 배포확인을 위한 연습

---
+++ server/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+
+const serverPort = 80;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+    credentials: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send('Hello Server!');
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Not Found!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({
+    message: 'Internal Server Error',
+    stacktrace: err.toString(),
+  });
+});
+
+app.listen(serverPort, () => {
+  console.log(`서버 연결 성공 🍎`);
+});
