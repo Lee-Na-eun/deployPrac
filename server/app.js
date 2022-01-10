dac318135a453ecc5d0d38e3740268257e0cb2fb
Lee Naeun <nanni4287@gmail.com>
2022-01-10 16:29:11 +0900

    [Server] server port 변경

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
 
-const serverPort = 80;
+const serverPort = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
